c76651efbae2927b3d613debba7aa3b69268adc2
Sabiha <66533748+telads@users.noreply.github.com>
2024-05-30 10:22:17 +0000

    recent changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myHeadings = document.querySelectorAll("h1");
+const firstH1 = myHeadings[0]
+console.log(myHeadings)
+console.log(firstH1)
+const firstH1text = firstH1.innerText
+console.log(firstH1text)
+myHeadings.devon = "go away"
+myHeadings.textContent = "Hello world!";
