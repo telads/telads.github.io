f58b567115eb7c3838b25cd601f09385c07586d6
Sabiha <66533748+telads@users.noreply.github.com>
2024-06-13 10:39:35 +0000

    Challenge accepted

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,18 @@
 const myHeadings = document.querySelectorAll("h1");
 const firstH1 = myHeadings[0]
-console.log(myHeadings)
-console.log(firstH1)
-const firstH1text = firstH1.innerText
-console.log(firstH1text)
-myHeadings.devon = "go away"
-myHeadings.textContent = "Hello world!";
+const secondH1 = myHeadings[1]
+const thirdH1 = myHeadings[2]
+const fourthH1 = myHeadings[3]
+const fifthH1 = myHeadings[4]
+const sixthH1 = myHeadings[5]
+// console.log({firstH1, secondH1})
+// const firstH1text = firstH1.innerText
+// console.log(firstH1text)
+// myHeadings.devon = "go away"
+// myHeadings.textContent = "Hello world!";
+firstH1.innerText = "1"
+secondH1.innerText = "2"
+thirdH1.innerText = "3"
+fourthH1.innerText = "4"
+fifthH1.innerText = "5"
+sixthH1.innerText = "6"
