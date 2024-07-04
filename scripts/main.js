00f64c0f2586c8cfccf012dd8b215f3ff3fecdaf
Sabiha <66533748+telads@users.noreply.github.com>
2024-07-04 11:13:01 +0000

    working with arrays

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,30 @@
 const myHeadings = document.querySelectorAll("h1");
-const firstH1 = myHeadings[0]
-const secondH1 = myHeadings[1]
-const thirdH1 = myHeadings[2]
-const fourthH1 = myHeadings[3]
-const fifthH1 = myHeadings[4]
-const sixthH1 = myHeadings[5]
-// console.log({firstH1, secondH1})
-// const firstH1text = firstH1.innerText
-// console.log(firstH1text)
-// myHeadings.devon = "go away"
 // myHeadings.textContent = "Hello world!";
-firstH1.innerText = "1"
-secondH1.innerText = "2"
-thirdH1.innerText = "3"
-fourthH1.innerText = "4"
-fifthH1.innerText = "5"
-sixthH1.innerText = "6"
+// myHeadings.forEach((element) => console.log(element));
+myHeadings.forEach((myHeading) => myHeading.textContent = "Hello world");
+
+
+// const firstH1 = myHeadings[0]
+// const secondH1 = myHeadings[1]
+// const thirdH1 = myHeadings[2]
+// const fourthH1 = myHeadings[3]
+// const fifthH1 = myHeadings[4]
+// const sixthH1 = myHeadings[5]
+
+
+// const myNumbers = ["one", "two", "three", "four", "five", "six"];
+
+// // firstH1.innerText = myNumbers[0]
+// // secondH1.innerText = myNumbers[1]
+// // thirdH1.innerText = myNumbers[2]
+// // fourthH1.innerText = myNumbers[3]
+// // fifthH1.innerText = myNumbers[4]
+// // sixthH1.innerText = myNumbers[5]
+
+// // array1.forEach((element) => console.log(element));
+// myHeadings.forEach((heading) => console.log(heading.innerText));
+
+// //challenge: create a variable called myNumber and use myNumber for the innerText
+// //firstH1.innerText = myNumber 
+
+// challenge execute an array loop 
